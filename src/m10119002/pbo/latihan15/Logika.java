7b80ecb5374eea22367398b689e2df8bc3fd1e98
m10119002 <firman.10119002@mahasiswa.unikom.ac.id>
2020-10-21 01:55:03 +0700

    21 Rabu 2020

---
+++ src/m10119002/pbo/latihan15/Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m10119002.pbo.latihan15;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : IF-1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program Logika
+ * class
+ * 
+ */
+public class Logika {
+    
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
